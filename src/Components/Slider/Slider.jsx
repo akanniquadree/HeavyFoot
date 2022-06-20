f6c98d5cd05f5c77e21c58a921e93d8079bd32c5
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2022-06-20 10:37:23 +0100

    Slider Updated

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -7,36 +7,72 @@ import { ArrowLeft, ArrowRight } from "@material-ui/icons"
 
 
 export default function Slider({slides}) {
-    const [current, setCurrent] = useState(0)
+    const [current, setCurrent] = useState(1)
     const length = slides.length
+    const timeout = useRef(null)
     const nextSlide = () =>{
-      setCurrent(current === length - 1 ? 0 : current +1)
-      console.log(current)
+      if(timeout.current){
+        clearTimeout(timeout.current)
+      }
+      if(current !== length){
+        setCurrent(current + 1)
+      }else if(current === length){
+        setCurrent(1)
+      }
     }
     const prevSlide = () =>{
-      setCurrent(current === 0 ? length - 1 : current -1)
-      console.log(current)
+      if(timeout.current){
+        clearTimeout(timeout.current)
+      }
+      if(current !== 1){
+        setCurrent(current - 1)
+      }else if(current === 1){
+        setCurrent(length)
+      }
+    }
+    const moveDot = (index) =>{
+      if(timeout.current){
+        clearTimeout(timeout.current)
+      }
+      setCurrent(index)
     }
     useEffect(()=>{
-    },[])
+      const nextSlide = () =>{
+        if(current !== length){
+          setCurrent(current + 1)
+        }else if(current === length){
+          setCurrent(1)
+        }
+      }
+      timeout.current = setTimeout(nextSlide, 3000)
+      return function (){
+        if(timeout.current){
+          clearTimeout(timeout.current)
+        }
+      }
+      
+    },[current, length])
     
   return (
     <div className="slider">
         <div className="sliderWrapper">
             {
               slides.map((item, index)=>(
-                  <div className="carousel"key={index}>
-                    {index === current && (
+                  <div className={current === index + 1 ? "carousel active-anim": "carousel"}key={index}>
     
                         <img src={item.image} alt="" />
-         
-                    )}
+
                   </div>
               
               ))
             }
             <ArrowLeft className="Arrow" onClick={prevSlide}/>
             <ArrowRight className="Arrow" style={{marginLeft:"50px"}} onClick={nextSlide}/>
+            <div className="contDot">
+              {Array.from({length:length}).map((item, index)=>(
+                <div className={ current === index +1 ? "dots active": "dots"} onClick={()=>{moveDot(index + 1)}}></div>
+              ))}
+            </div>
         </div>
     </div>
   )

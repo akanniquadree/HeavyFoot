c2b19cddb936a547ad1b710fff817fead02818db
akanniquadree <akanniquadry7@gmail.com>
2022-09-24 00:25:51 -0700

    slight Changes in home

--- src/Components/Promotion/Promotion.jsx
+++ src/Components/Promotion/Promotion.jsx
@@ -5,8 +5,15 @@ export default function Promotion() {
   return (
     <div className="promotion">
         <div className="promotionWrapper">
-            <div className="promotionTop" style={{marginBottom:"10px"}}>
-              <img src="/Images/promotion.gif" alt="" />
+            <div className="promotionTop">
+              <img src="/Images/ad.png" alt="" />
+            </div>
+            <div className="promotionBottom">
+                <h4>GAMING COLLECTION</h4>
+                <h2 style={{color:"#f4a51c"}}>Playstation 4</h2>
+                <h2>Dualshock Controller</h2> 
+                <span>sale price: $125</span>
+                <button>Purchase Now</button>
             </div>
         </div>
         
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -25,6 +25,7 @@ export default function Home() {
       <HomeFashion/>
       <HomeGadget/>
       <HomeGadgetTwo/>
+      <Promotion/>
       {/* <Footer/> */}
     </>
   )
